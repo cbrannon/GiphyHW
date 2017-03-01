d7e17fe289c4951ecece561227393492470eb561
Cody Brannon <codydbrannon@gmail.com>
2017-03-01 16:10:11 -0500

    ajax call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,10 @@ $(document).ready(function(){
 
         setInitialTopics: function(buttonTopics) {
             var topicArray = buttonTopics;
-            function setTopic(topicsToBuild) {
-                for (var topicIndex = 0; topicIndex < topicsToBuild.length; topicIndex++) {
-                    var button = topicsToBuild[topicIndex];
-                    giphy.setButton(button);
-                }
+            for (var topicIndex = 0; topicIndex < topicArray.length; topicIndex++) {
+                var button = topicArray[topicIndex];
+                giphy.setButton(button);
             }
-            return setTopic(topicArray);
         },
 
         setButton: function(button) {
@@ -24,7 +21,7 @@ $(document).ready(function(){
             function build(topic) {
                 var newButton = $("<button>");
                 newButton.attr("type", "button");
-                newButton.addClass("btn btn-primary");
+                newButton.addClass("btn btn-primary anime");
                 newButton.text(topic.toLowerCase());
                 $("#animeButtons").append(newButton);
                 console.log("New Button: " + topic);
@@ -37,9 +34,9 @@ $(document).ready(function(){
             var topic = newTopic.toLowerCase();
             function pushTopic(topicToAdd) {
                 if (giphy.topics.indexOf(topicToAdd) == -1 && topicToAdd != "" && jQuery.trim(topicToAdd).length != 0) {
+                    $("#anime-input").val("");
                     giphy.topics.push(topicToAdd);
                     giphy.setButton(topicToAdd);
-                    console.log(giphy.topics);
                 }
             }
             return pushTopic(topic);
@@ -47,17 +44,27 @@ $(document).ready(function(){
     }
 
     giphy.setInitialTopics(giphy.topics);
+    
     $(window).keydown(function(event){
-        if(event.keyCode == 13) {
-        event.preventDefault();
-        return false;
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            return false;
         }
     });
 
     $("#addAnime").on("click", function(){
-        var newTopic = $("#anime-input").val();
-        giphy.checkTopicPush(newTopic);
-        $("#anime-input").val("");
-        console.log("Button Pressed");
+        giphy.checkTopicPush($("#anime-input").val());
+    });
+
+    $(".anime").on("click", function(){
+        var currentTopic = $(this).text();
+        currentTopic = currentTopic.replace(/ /g, '+');
+        console.log("Current topic is: " + currentTopic);
+        $.ajax({
+            url: "http://api.giphy.com/v1/gifs/search?q=" + currentTopic + "&api_key=dc6zaTOxFJmzC",
+            method: "GET",
+        }).done(function(response) {
+            console.log(response);
+        });
     });
 });
