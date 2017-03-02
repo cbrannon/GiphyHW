97d96215a284a71a7370a0bb14c3c37745fd0e05
Cody Brannon <codydbrannon@gmail.com>
2017-03-02 12:24:34 -0500

    Updated styling and added comments to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,39 +6,33 @@ $(document).ready(function(){
             "gundam", "princess mononoke", "mob psycho 100",                  
             "psycho pass", "outlaw star", "cowboy bebop", 
             "death note", "rurouni kenshin", "dragon ball z",
-            "sword art online", "spirited away", "flcl"],
+            "sword art online", "spirited away", "flcl", 
+            "pom poko", "my hero academia"],
         
         apiData: null,
-        
-        
-        
-        pushTopic: function (topic) {
+
+        // Set topic if not current topic, empty string, or string of spaces.
+        setTopic: function (topic) {
             if (giphy.topics.indexOf(topic) == -1 && topic != "" && jQuery.trim(topic).length != 0) {
                 $("#anime-input").val("");
                 giphy.topics.push(topic);
                 giphy.setButton(topic);
             }
         },
-        
-        checkTopicPush: function (newTopic) {
-            var topic = newTopic.toLowerCase();
-            function pushTopic(topicToAdd) {
-                
-            }
-            return pushTopic(topic);
-        },
 
+        // Set button for a topic.
         setButton: function (button) {
             function build(topic) {
                 var newButton = $("<button>");
                 newButton.attr("type", "button");
-                newButton.addClass("btn btn-primary anime");
+                newButton.addClass("btn anime");
                 newButton.text(topic.toLowerCase());
                 $("#animeButtons").append(newButton);
             }
             return build(button);
         },
 
+        // Set initial topics.
         setInitialTopics: function (buttonTopics) {
             for (var topicIndex = 0; topicIndex < buttonTopics.length; topicIndex++) {
                 var button = buttonTopics[topicIndex];
@@ -46,7 +40,9 @@ $(document).ready(function(){
             }
         },
 
+        // Get API data for topic.
         getInfo: function (topic) {
+            $("#anime").empty();
             $.ajax({
                 url: "http://api.giphy.com/v1/gifs/search?q=" + topic + "&limit=10&api_key=dc6zaTOxFJmzC",
                 method: "GET",
@@ -55,7 +51,8 @@ $(document).ready(function(){
                 console.log(giphy.apiData);
                 gifToDom(giphy.apiData);
             });
-            
+
+            // Loop through each gif object and call setDom on each.
             function gifToDom(data) {
                 for (var item = 0; item < data.length; item++) {
                     var thumbnail = data[item].images.fixed_height_still.url;
@@ -65,16 +62,19 @@ $(document).ready(function(){
             }   
         },
 
+        // Set DOM elements for a gif.
         setDom: function (image, rating, item) {
             var newGif = $("<div>");
             var ratElement = $("<p>");
             var imgElement = $("<img>");
 
+            // Set rating DOM element.
             function setRating(ele, rating) {
                 ele.text("Rating: " + rating);
                 newGif.append(ele);
             }
 
+            // Set image DOM element.
             function setImage(ele, img, item) {
                 ele.addClass("imgs");
                 ele.attr("src", img);
@@ -82,6 +82,7 @@ $(document).ready(function(){
                 newGif.append(ele);
             }
 
+            // Set element to DOM.
             function setElement() {
                 newGif.addClass("gifs");
                 $("#anime").append(newGif);
@@ -92,21 +93,22 @@ $(document).ready(function(){
             setElement();
         },
 
+        // Set image source
         setSrc: function (index, currentSrc) {
             var gifItem = giphy.apiData[index];
             var itemId = "#" + index;
 
+            // Set to image
             function setImgSrc(id) {
                 $(id).attr("src", gifItem.images.fixed_height_still.url);
-                console.log(gifItem.images.fixed_height_still.url);
             }
 
+            // Set to gif
             function setGifSrc(id) {
                 $(id).attr("src", gifItem.images.fixed_height.url);
-                console.log(gifItem.images.fixed_height_still.url);
-
             }
 
+            // Check if current image source is the image or gif
             if (gifItem.images.fixed_height_still.url == currentSrc) {
                 setGifSrc(itemId);
             } else {
@@ -116,7 +118,8 @@ $(document).ready(function(){
     }
 
     giphy.setInitialTopics(giphy.topics);
-    
+
+    // Prevents enter key press from reloading page.
     $(window).keydown(function(event){
         if (event.keyCode == 13) {
             event.preventDefault();
@@ -124,20 +127,21 @@ $(document).ready(function(){
         }
     });
 
+    // Push new topic to array and get API data for it when pushing Submit
     $("#addAnime").on("click", function () {
-        $("#anime").empty();
         var newItem = $("#anime-input").val();
-        giphy.pushTopic(newItem);
+        giphy.setTopic(newItem);
         giphy.getInfo(newItem);
     });
 
+    // Get API data when pressing a topic button
     $("#animeButtons").on("click", ".anime", function () {
-        $("#anime").empty();
         var currentTopic = $(this).text();
         currentTopic = currentTopic.replace(/ /g, '+');
         giphy.getInfo(currentTopic);
     });
 
+    // Get gif index and image source when clicking a gif
     $("#anime").on("click", ".gifs .imgs", function () {
         var gifIndex = $(this).attr('id');
         var gifSrc = $(this).attr('src');
