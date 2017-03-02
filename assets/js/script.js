10b202e71430aa6b56b54c5e31a22067fb03c8ee
Cody Brannon <codydbrannon@gmail.com>
2017-03-01 20:33:26 -0500

    Push elements to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,79 @@
 $(document).ready(function(){
     var giphy = {
 
-        apiKey: "dc6zaTOxFJmzC",
-        topics: ["one punch man", "attack on titan", "akira", 
-              "gundam", "princess mononoke", "mob psycho 100", 
-              "psycho pass", "outlaw star", "cowboy bebop", 
-              "death note", "rurouni kenshin", "dragon ball z",
-              "sword art online", "spirited away", "FLCL"],
-
-        setInitialTopics: function(buttonTopics) {
-            var topicArray = buttonTopics;
-            for (var topicIndex = 0; topicIndex < topicArray.length; topicIndex++) {
-                var button = topicArray[topicIndex];
-                giphy.setButton(button);
+        topics: ["my neighbor totoro", "full metal alchemist",
+                 "one punch man", "attack on titan", "akira", 
+                 "gundam", "princess mononoke", "mob psycho 100", 
+                 "psycho pass", "outlaw star", "cowboy bebop", 
+                 "death note", "rurouni kenshin", "dragon ball z",
+            "sword art online", "spirited away", "flcl"],
+        
+        pushTopic: function (topic) {
+            if (giphy.topics.indexOf(topic) == -1 && topic != "" && jQuery.trim(topic).length != 0) {
+                $("#anime-input").val("");
+                giphy.topics.push(topic);
+                giphy.setButton(topic);
             }
         },
+        
+        checkTopicPush: function (newTopic) {
+            var topic = newTopic.toLowerCase();
+            function pushTopic(topicToAdd) {
+                
+            }
+            return pushTopic(topic);
+        },
 
-        setButton: function(button) {
-            var newButtonTopic = button;
+        setButton: function (button) {
             function build(topic) {
                 var newButton = $("<button>");
                 newButton.attr("type", "button");
                 newButton.addClass("btn btn-primary anime");
                 newButton.text(topic.toLowerCase());
                 $("#animeButtons").append(newButton);
-                console.log("New Button: " + topic);
-
             }
-            return build(newButtonTopic);
+            return build(button);
         },
 
-        checkTopicPush: function(newTopic) {
-            var topic = newTopic.toLowerCase();
-            function pushTopic(topicToAdd) {
-                if (giphy.topics.indexOf(topicToAdd) == -1 && topicToAdd != "" && jQuery.trim(topicToAdd).length != 0) {
-                    $("#anime-input").val("");
-                    giphy.topics.push(topicToAdd);
-                    giphy.setButton(topicToAdd);
-                }
+        setInitialTopics: function (buttonTopics) {
+            for (var topicIndex = 0; topicIndex < buttonTopics.length; topicIndex++) {
+                var button = buttonTopics[topicIndex];
+                giphy.setButton(button);
             }
-            return pushTopic(topic);
+        },
+
+        getInfo: function (gifObject) {
+            for (var item = 0; item < gifObject.length; item++) {
+                var thumbnail = gifObject[item].images.fixed_height.url;
+                var rating = gifObject[item].rating;
+                giphy.setDom(thumbnail, rating, item);
+            }
+        },
+
+        setDom: function (image, rating, item) {
+            var newGif = $("<div>");
+            var ratElement = $("<p>");
+            var imgElement = $("<img>");
+
+            function setRating(ele, rating) {
+                ele.text("Rating: " + rating);
+                newGif.append(ele);
+            }
+
+            function setImage(ele, img, item) {
+                ele.attr("src", img);
+                ele.attr("id", item);
+                newGif.append(ele);
+            }
+
+            function setElement() {
+                newGif.addClass("gifs");
+                $("#anime").append(newGif);
+                setRating(ratElement, rating);
+                setImage(imgElement, image, item);
+            }
+
+            setElement();
         },
     }
 
@@ -53,18 +87,19 @@ $(document).ready(function(){
     });
 
     $("#addAnime").on("click", function(){
-        giphy.checkTopicPush($("#anime-input").val());
+        giphy.pushTopic($("#anime-input").val());
     });
 
-    $(".anime").on("click", function(){
+    $("#animeButtons").on("click", ".anime", function () {
+        $("#anime").empty();
         var currentTopic = $(this).text();
         currentTopic = currentTopic.replace(/ /g, '+');
         console.log("Current topic is: " + currentTopic);
         $.ajax({
-            url: "http://api.giphy.com/v1/gifs/search?q=" + currentTopic + "&api_key=dc6zaTOxFJmzC",
+            url: "http://api.giphy.com/v1/gifs/search?q=" + currentTopic + "&limit=10&api_key=dc6zaTOxFJmzC",
             method: "GET",
         }).done(function(response) {
-            console.log(response);
+            giphy.getInfo(response.data);
         });
     });
 });
